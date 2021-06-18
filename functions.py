ec29f31e7a8b533fe3d38cd2bfefa65a87167a95
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2021-06-18 12:34:39 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -41,7 +41,7 @@ def gen_corpus(file_loc):
         file = file.replace("\\", "/")
         with open (file, "r", encoding="utf-8", errors='ignore') as f:
             text = f.read()
-            segs =  text.split("\n\n")
+            segs =  text.split("\n\n\n")
             for seg in segs:
                 original = seg.strip()+"."
                 original = original.replace("\n", "<br>")
