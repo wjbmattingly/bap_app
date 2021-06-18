2830a7a4631d6ee10f17665ba2bdc1ec1fc15acc
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2021-06-18 12:29:38 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -39,7 +39,7 @@ def gen_corpus(file_loc):
     final_files = []
     for file in files:
         file = file.replace("\\", "/")
-        with open (file, "r", encoding="utf-8") as f:
+        with open (file, "r", encoding="utf-8", errors='ignore') as f:
             text = f.read()
             segs =  text.replace("\n\n", " ").split(".")
             for seg in segs:
